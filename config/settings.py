0189d5f469fbce5a3d6889e43a868fcde7b63531
jass <a123@jasss-MacBook-Pro.local>
2026-06-22 09:26:11 -0400

    configure Namecheap SMTP email notifications

--- config/settings.py
+++ config/settings.py
@@ -152,11 +152,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Namecheap Private Email SMTP
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "mail.privateemail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER ="roselineokosun@tmnigeria.com"
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = env_config("SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 LANDING_ADMIN_EMAIL = env_config("LANDING_ADMIN_EMAIL", default=DEFAULT_FROM_EMAIL)
